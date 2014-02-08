4cc21eda790b1d51ebb9383ae97acc166ae71fff
rafaelav <rafaela.voiculescu@gmail.com>
2014-02-08 16:18:31 +0100

    made EndOfCycleHandler more programmer-friendly :)

--- greenself/src/com/greenself/EndOfCycleHandler.java
+++ greenself/src/com/greenself/EndOfCycleHandler.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.greenself.constants.Constants;
+import com.greenself.constants.Constants.Type;
 import com.greenself.daogen.Task;
 import com.greenself.dbhandlers.DBManager;
 import com.greenself.observers.ObserverPatternWatched;
@@ -37,104 +38,57 @@ public class EndOfCycleHandler extends ObserverPatternWatched {
 		// dailyTasksFragment = fragment;
 		SharedPreferences prefs = context.getSharedPreferences(Constants.APP,
 				Context.MODE_PRIVATE);
-		Long lastDailyUpdate = prefs.getLong(Constants.LAST_DAILY_UPDATE, 0);
-		Date now = new Date();
-
-		if (now.getTime() / 1000 - lastDailyUpdate >= Constants.BETWEEN_DAYS) {
-			endOfCycleUpdates(context);
-		}
-	}
-
-	public void endOfCycleUpdates(Context context) {
-		// save to history the tasks that have been completed (daily)
-		TaskHandler.archiveCompletedTasks(context, Constants.Type.DAILY);
-		log.info("Now in History: "
-				+ DBManager.getInstance(context).getDaoSession()
-						.getTaskHistoryDao().loadAll().toString());
-
-		// drop completed tasks (daily)
-		TaskHandler.dropCompletedTasks(context, Constants.Type.DAILY);
-
-		// drop daily tasks from active
-		TaskHandler.dropNotCompletedTasksFromActive(context,
-				Constants.Type.DAILY);
-		
-		// generating new daily tasks for next cycle
-		TaskHandler.generateNewTasks(context, Constants.NO_OF_DAILY_TASKS,
-				Constants.Type.DAILY);
-
-		// get last updates of monthly/weekly tasks
-		SharedPreferences prefs = context.getSharedPreferences(Constants.APP,
-				Context.MODE_PRIVATE);
+		Long lastDailyUpdateTime = prefs
+				.getLong(Constants.LAST_DAILY_UPDATE, 0);
 		Long lastWeeklyUpdateTime = prefs.getLong(Constants.LAST_WEEKLY_UPDATE,
 				0);
 		Long lastMonthlyUpdateTime = prefs.getLong(
 				Constants.LAST_MONTHLY_UPDATE, 0);
 
-		int weeklyTasksToReg = determineNoOfWeeklyTasksToReg(context,
-				lastWeeklyUpdateTime);
+		Date now = new Date();
 
-		int monthlyTasksToReg = determineNoOfMonthlyTasksToReg(context,
-				lastMonthlyUpdateTime);
-
-		// drop weekly tasks if needed (either all completed or time to change
-		// them) & regenerate them for next cycle
-		if (weeklyTasksToReg == Constants.NO_OF_WEEKLY_TASKS) {
-			TaskHandler.archiveCompletedTasks(context, Constants.Type.WEEKLY);
-			log.info("Now in History: "
-					+ DBManager.getInstance(context).getDaoSession()
-							.getTaskHistoryDao().loadAll().toString());
-			// drop completed tasks
-			TaskHandler.dropCompletedTasks(context, Constants.Type.WEEKLY);
-			// drop not completed
-			TaskHandler.dropNotCompletedTasksFromActive(context,
-					Constants.Type.WEEKLY);
-			TaskHandler.generateNewTasks(context, Constants.NO_OF_WEEKLY_TASKS,
-					Constants.Type.WEEKLY);
+		if (now.getTime() / 1000 - lastDailyUpdateTime >= Constants.BETWEEN_DAYS) {
+			endOfCycleUpdates(context, Constants.Type.DAILY);
 		}
+		if (now.getTime() / 1000 - lastWeeklyUpdateTime >= Constants.BETWEEN_WEEKS) {
+			endOfCycleUpdates(context, Constants.Type.WEEKLY);
+		}
+		if (now.getTime() / 1000 - lastMonthlyUpdateTime >= Constants.BETWEEN_MONTHS) {
+			endOfCycleUpdates(context, Constants.Type.MONTHLY);
+		}
+	}
 
-		// drop monthly tasks if needed (either all completed or time to change
-		// them) & regenerate them for next cycle
-		if (monthlyTasksToReg == Constants.NO_OF_MONTHLY_TASKS) {
-			TaskHandler.archiveCompletedTasks(context, Constants.Type.MONTHLY);
-			log.info("Now in History: "
-					+ DBManager.getInstance(context).getDaoSession()
-							.getTaskHistoryDao().loadAll().toString());
-			// drop completed tasks
-			TaskHandler.dropCompletedTasks(context, Constants.Type.MONTHLY);
-			// drop not completed
-			TaskHandler.dropNotCompletedTasksFromActive(context,
-					Constants.Type.MONTHLY);
+	public void endOfCycleUpdates(Context context, Type type) {
+		// save to history the tasks that have been completed 
+		TaskHandler.archiveCompletedTasks(context, type);
+		log.info("Now in History: "
+				+ DBManager.getInstance(context).getDaoSession()
+						.getTaskHistoryDao().loadAll().toString());
+
+		// drop completed tasks
+		TaskHandler.dropCompletedTasks(context, type);
+
+		// drop tasks from active
+		TaskHandler.dropNotCompletedTasksFromActive(context, type);
+
+		// generating new daily tasks for next cycle
+		switch (type) {
+		case DAILY:
+			TaskHandler.generateNewTasks(context, Constants.NO_OF_DAILY_TASKS,
+					type);
+			break;
+		case WEEKLY:
+			TaskHandler.generateNewTasks(context, Constants.NO_OF_WEEKLY_TASKS,
+					type);
+			break;
+		case MONTHLY:
 			TaskHandler.generateNewTasks(context,
-					Constants.NO_OF_MONTHLY_TASKS, Constants.Type.MONTHLY);
+					Constants.NO_OF_MONTHLY_TASKS, type);
+			break;
 		}
 
 		// notify changes to listeners
 		notifyListeners();
-	}
-
-	private int determineNoOfMonthlyTasksToReg(Context context, Long lastUpdate) {
-		Date now = new Date();
-		Long betweenUpdates = now.getTime() / 1000 - lastUpdate;
-		if (betweenUpdates >= Constants.BETWEEN_MONTHS) {
-			return Constants.NO_OF_MONTHLY_TASKS;
-		} else {
-			List<Task> tasks = TaskHandler.getNotDoneTypeTasks(context,
-					Constants.Type.WEEKLY);
-			return Constants.NO_OF_MONTHLY_TASKS - tasks.size();
-		}
-	}
-
-	private int determineNoOfWeeklyTasksToReg(Context context, Long lastUpdate) {
-		Date now = new Date();
-		Long betweenUpdates = now.getTime() / 1000 - lastUpdate;
-		if (betweenUpdates >= Constants.BETWEEN_WEEKS) {
-			return Constants.NO_OF_WEEKLY_TASKS;
-		} else {
-			List<Task> tasks = TaskHandler.getNotDoneTypeTasks(context,
-					Constants.Type.WEEKLY);
-			return Constants.NO_OF_WEEKLY_TASKS - tasks.size();
-		}
 	}
 
 	// TODO - check
