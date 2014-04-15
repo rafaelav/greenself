bbbbc2adc36d29bef099d2a003084f1ab8c3d087
rafaelav <rafaela.voiculescu@gmail.com>
2014-04-15 18:41:47 +0200

    Updates on how the score looks and added clickable.

--- greenself/src/com/greenself/StatisticsFragment.java
+++ greenself/src/com/greenself/StatisticsFragment.java
@@ -1,10 +1,13 @@
 package com.greenself;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.greenself.events.EventsBusFactory;
@@ -13,48 +16,92 @@ import com.greenself.events.SingleTaskChangeEvent;
 import com.greenself.handlers.ScoreHandler;
 import com.squareup.otto.Subscribe;
 
-public class StatisticsFragment extends Fragment{
-	
+public class StatisticsFragment extends Fragment {
+
 	private TextView statsDailyTasksScore;
 	private TextView statsWeeklyTasksScore;
 	private TextView statsMonthlyTasksScore;
 	private TextView statsOverallScore;
-	
+	private RelativeLayout fullScoreLayout;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.statistics, null);
-		
+
 		// calculate statistics
 		ScoreHandler.getInstance(getActivity()).updateAllStatistics();
-		
+
 		// update scores published on fragment
-		statsDailyTasksScore = (TextView) view.findViewById(R.id.statsDailyTasksScore);
-		statsWeeklyTasksScore = (TextView) view.findViewById(R.id.statsWeeklyTasksScore);
-		statsMonthlyTasksScore = (TextView) view.findViewById(R.id.statsMonthlyTasksScore);
-		statsOverallScore = (TextView) view.findViewById(R.id.statsOverallScore);
-		
+		statsDailyTasksScore = (TextView) view
+				.findViewById(R.id.statsDailyTasksScore);
+		statsWeeklyTasksScore = (TextView) view
+				.findViewById(R.id.statsWeeklyTasksScore);
+		statsMonthlyTasksScore = (TextView) view
+				.findViewById(R.id.statsMonthlyTasksScore);
+		statsOverallScore = (TextView) view
+				.findViewById(R.id.statsOverallScore);
+		fullScoreLayout = (RelativeLayout) view
+				.findViewById(R.id.fullScoreLayout);
+		fullScoreLayout.setClickable(true);
+
+		fullScoreLayout.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(
+						getActivity());
+				builder.setMessage("Clicked!");
+				builder.setPositiveButton(R.string.ok,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// User clicked OK button
+							}
+						});
+				AlertDialog dialog = builder.create();
+				dialog.show();
+
+			}
+		});
 		// register for updates from bus
 		EventsBusFactory.getInstance().register(this);
-		
+
 		return view;
 	}
-	
-	@Subscribe public void updateStatisticsForMultipleTasksChange(MultipleTasksChangeEvent event) {
+
+	@Subscribe
+	public void updateStatisticsForMultipleTasksChange(
+			MultipleTasksChangeEvent event) {
 		ScoreHandler.getInstance(getActivity()).updateAllStatistics();
-		
-		statsDailyTasksScore.setText(""+ScoreHandler.getInstance(getActivity()).getNumberOfCompletedDailyTasks());
-		statsWeeklyTasksScore.setText(""+ScoreHandler.getInstance(getActivity()).getNumberOfCompletedWeeklyTasks());
-		statsMonthlyTasksScore.setText(""+ScoreHandler.getInstance(getActivity()).getNumberOfCompletedMonthlyTasks());
-		statsOverallScore.setText(ScoreHandler.getInstance(getActivity()).getOverallScore()+"");
+
+		statsDailyTasksScore.setText(""
+				+ ScoreHandler.getInstance(getActivity())
+						.getNumberOfCompletedDailyTasks());
+		statsWeeklyTasksScore.setText(""
+				+ ScoreHandler.getInstance(getActivity())
+						.getNumberOfCompletedWeeklyTasks());
+		statsMonthlyTasksScore.setText(""
+				+ ScoreHandler.getInstance(getActivity())
+						.getNumberOfCompletedMonthlyTasks());
+		statsOverallScore.setText(ScoreHandler.getInstance(getActivity())
+				.getOverallScore() + "");
 	}
-	
-	@Subscribe public void updateStatisticsForSingleTaskChange(SingleTaskChangeEvent event) {
-		ScoreHandler.getInstance(getActivity()).updateStatisticsForSingleTaskChange(event.getChangedTask());
-		
-		statsDailyTasksScore.setText(""+ScoreHandler.getInstance(getActivity()).getNumberOfCompletedDailyTasks());
-		statsWeeklyTasksScore.setText(""+ScoreHandler.getInstance(getActivity()).getNumberOfCompletedWeeklyTasks());
-		statsMonthlyTasksScore.setText(""+ScoreHandler.getInstance(getActivity()).getNumberOfCompletedMonthlyTasks());
-		statsOverallScore.setText(ScoreHandler.getInstance(getActivity()).getOverallScore()+"");		
+
+	@Subscribe
+	public void updateStatisticsForSingleTaskChange(SingleTaskChangeEvent event) {
+		ScoreHandler.getInstance(getActivity())
+				.updateStatisticsForSingleTaskChange(event.getChangedTask());
+
+		statsDailyTasksScore.setText(""
+				+ ScoreHandler.getInstance(getActivity())
+						.getNumberOfCompletedDailyTasks());
+		statsWeeklyTasksScore.setText(""
+				+ ScoreHandler.getInstance(getActivity())
+						.getNumberOfCompletedWeeklyTasks());
+		statsMonthlyTasksScore.setText(""
+				+ ScoreHandler.getInstance(getActivity())
+						.getNumberOfCompletedMonthlyTasks());
+		statsOverallScore.setText(ScoreHandler.getInstance(getActivity())
+				.getOverallScore() + "");
 	}
 }
